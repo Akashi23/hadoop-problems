6e1aa7ae17eaf02e63361af8340883d135f397f7
Akashi <dan.zh.99@gmail.com>
2021-11-17 19:03:01 +0600

    Not First

--- generator.py
+++ generator.py
@@ -2,6 +2,6 @@
 
 import random
 
-numbers = [random.randint(0, 16) for x in range(1000)]
+numbers = [str(random.randint(0, 16)) for x in range(1000)]
 
-print(' '.join(str(numbers)))
+print(' '.join(numbers))
